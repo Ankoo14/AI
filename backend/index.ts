128e9bf076fad49fa92a07ae3e844cbe0731e9cf
Ankur Yadav <ankur.yadav.data@gmail.com>
2025-12-09 19:47:33 +0530

    ok

--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,19 @@
 console.log("Hello via Bun!");
 import express from "express";
+import { CreateChatSchema } from "./types";
 
 const app = express();
+app.use(express.json());
+
 app.post("/chat", (req, res) => {
     const {success, data} = CreateChatSchema.safeParse(req.body);
+    if (!success) {
+        res.status(411).json({
+            message: "Incorrect Inputs"
+        })
+        return
+    }    
+    const { message } = data;
   
 });
 const port = 3000;
